8af85c87df2c49159af6756e1ba3ccfb5338421e
sebas1803 <52021716+sebas1803@users.noreply.github.com>
2022-04-01 01:52:15 -0500

    added facebook and google auth

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,29 @@
+import NextAuth from 'next-auth';
+import FacebookProvider from 'next-auth/providers/facebook';
+import GoogleProvider from 'next-auth/providers/google';
+import Auth0Provider from "next-auth/providers/auth0";
+
+export default NextAuth({
+  providers: [
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_ID,
+      clientSecret: process.env.FACEBOOK_SECRET,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code"
+        }
+      }
+    }),
+    Auth0Provider({
+      clientId: process.env.AUTH0_ID,
+      clientSecret: process.env.AUTH0_SECRET,
+      issuer: process.env.AUTH0_ISSUER
+    }),
+  ],
+})
