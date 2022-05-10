e823b6f061c4e09c12dfd435d233103c18b5d620
Giordanov2 <giordanitrox@gmail.com>
2022-05-10 16:46:15 -0500

    Fixed Footer and SearchID page

--- components/footer.js
+++ components/footer.js
@@ -2,7 +2,7 @@
 export default function Footer() {
     return (
         <footer className="bg-white celular:text-sm md:text-base h-[120px]">
-            <div className='flex items-center px-12 py-9 border-t-2 border-gray-200 h-[120px]'>
+            <div className='flex items-center px-12 py-9 h-[120px]'>
                 <p className="flex text-left text-gray-400">© 2022 PotatoOntology, Inc. All rights reserved.</p>
             </div>
         </footer>
