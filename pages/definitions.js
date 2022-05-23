9ce3e6067c03bcf7e699833578e50ed3bd9ed625
Giordanov2 <giordanitrox@gmail.com>
2022-05-23 02:54:38 -0500

    Mejoras en la pantalla /definitions

--- pages/definitions.js
+++ pages/definitions.js
@@ -5,6 +5,8 @@ import { useEffect, useState } from 'react'
 import { baseUrl } from '../service/api'
 import dayjs from 'dayjs'
 import { useCallback } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
 export default function Definitions() {
 
@@ -72,10 +74,18 @@ export default function Definitions() {
                     <div className='py-12'>
                         <h1 className='font-bold celular:text-4xl md:text-5xl'>Conceptos guardados</h1>
                     </div>
+                    <div className='flex flex-row relative justify-left'>
+                    <div className='relative w-2/5 h-14'>
+                        <input className="w-full rounded-md px-3 border border-black h-10" type="text" placeholder='Buscar concepto...'></input>
+                        <div className='pointer-events-none absolute text-black inset-y-2 right-3'>
+                            <a><FontAwesomeIcon icon={faSearch} size="1x" /></a>
+                        </div>
+                    </div>
+                    </div>
                     <div className='flex celular:flex-col md:flex-row md:space-x-16'>
                         <div className='flex flex-col p-7 shadow-md rounded-lg h-1/2 w-1/3 my-2 border'>
                             <h1 className='font-bold text-lg'>
-                                Rango de fechas
+                                Filtrar por fechas
                             </h1>
                             <div className='py-2 font-semibold '>
                                 <div className='py-1'>
@@ -92,6 +102,9 @@ export default function Definitions() {
                                 <div className='py-1'>
                                     <input name="endDate" onChange={(e) => handleChangeDate(e)} className='w-full rounded-md px-3 outline-1 outline-offset-0 outline-gray-400 border border-gray-300 text-gray-600 h-10' type="date" />
                                 </div>
+                            </div>
+                            <div>
+                            <button  className='text-sm celular:w-full celular:h-10 md:w-40 md:h-10 bg-bluepotatohover text-white rounded-md font-medium'>Eliminar filtro</button>
                             </div>
                         </div>
                         <div className='flex flex-col'>
