5757b7b6ef68b8edfe953583892897d2eea7c428
Giordanov2 <giordanitrox@gmail.com>
2022-05-23 02:24:43 -0500

    Inclusion de mejoras /search/id

--- pages/search/[id].js
+++ pages/search/[id].js
@@ -4,6 +4,7 @@ import Footer from '../../components/footer'
 import Image from 'next/image'
 import { baseUrl } from '../../service/api'
 import { Alerting } from '../../utils/alert'
+import Link from "next/link"
 
 export default function Details({ plant }) {
 
@@ -49,6 +50,7 @@ export default function Details({ plant }) {
             <div className="flex md:flex-row celular:flex-col celular:items-center md:items-end md:space-x-20">
               <h1 className='font-extrabold text-2xl md:py-0 celular:py-4 text-center'>Enfermedades de la papa</h1>
               <button onClick={() => saveConcept(plant)} className='text-sm celular:w-full celular:h-10 md:w-40 md:h-10 bg-skyblue text-white rounded-md font-medium'>Guardar detalle</button>
+              <button onClick={() => saveConcept(plant)} className='text-sm celular:w-full celular:h-10 md:w-40 md:h-10 bg-bluepotatohover text-white rounded-md font-medium'>Eliminar detalle</button>
             </div>
             <div className='flex flex-col md:py-0 celular:py-10 md:w-[250px]'>
               <div className='flex justify-center md:h-7'>
@@ -60,7 +62,11 @@ export default function Details({ plant }) {
               </div>
             </div>
           </div>
-
+          <div>
+          <Link passHref href="/search">
+         <a href="replace" className=" hover:bg-gray-100 text-bluepotato px-3 py-1 rounded-md font-medium ">Regresar</a>
+          </Link>
+          </div>
           <div className='flex md:flex-row md:w-full md:justify-between celular:flex-col items-center'>
             <div className='flex flex-col md:w-1/2'>
               <h1 className='text-3xl px-10 pt-5 font-medium'>{plant.plantDiseaseName}</h1>
@@ -68,18 +74,29 @@ export default function Details({ plant }) {
                 <p>{plant.plantDiseaseDescription}</p>
               </div>
             </div>
-            <div className='flex md:flex-row celular:flex-col-reverse md:justify-between items-center h-72 shadow-lg rounded-xl w-1/2 m-6 celular:p-5 md:p-12'>
-              <div>
-                <ul className='text-skyblue'>
-                  <li className='hover:underline'>www.ontologiapapa.com</li>
-                  <li className='hover:underline'>www.ontologiapapa.com</li>
-                  <li className='hover:underline'>www.ontologiapapa.com</li>
-                  <li className='hover:underline'>www.ontologiapapa.com</li>
-                </ul>
-              </div>
+            <div className='flex md:flex-row celular:flex-col-reverse md:justify-between items-center h-72 shadow-lg rounded-xl w-100 m-6 celular:p-5 md:p-12'>
+
               <div>
                 <Image src="https://blush.design/api/download?shareUri=_nRDPd5Vo&w=800&h=800&fm=png"
                   alt="detail image" width={125} height={162} />
+              </div>
+              <div>
+              <button className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button" data-modal-toggle="popup-modal">
+                Conocer más
+              </button>
+
+              <div id="popup-modal" tabIndex="-1" className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 md:inset-0 h-modal md:h-full">
+                  <div className="relative p-4 w-full max-w-md h-full md:h-auto">
+                      <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                          <button type="button" className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-toggle="popup-modal">
+                              <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillrule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>  
+                          </button>
+                          <div className="p-6 text-center">                          
+                              <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Links con mayor información</h3>                          
+                          </div>
+                      </div>
+                  </div>
+              </div>
               </div>
             </div>
           </div>
