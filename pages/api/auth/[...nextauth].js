29b5de5718175f0e0c2e51354503cc9040b91d48
Sergio Bazan <drakeb2008@hotmail.com>
2022-05-29 14:45:33 -0500

    add header

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,9 +1,7 @@
-import NextAuth from 'next-auth';
-import FacebookProvider from 'next-auth/providers/facebook';
-import GoogleProvider from 'next-auth/providers/google';
+import NextAuth from "next-auth";
+import FacebookProvider from "next-auth/providers/facebook";
+import GoogleProvider from "next-auth/providers/google";
 import Auth0Provider from "next-auth/providers/auth0";
-import Credentials from 'next-auth/providers/credentials';
-import { baseUrl } from '../../../service/api';
 
 export default NextAuth({
   providers: [
@@ -18,38 +16,14 @@ export default NextAuth({
         params: {
           prompt: "consent",
           access_type: "offline",
-          response_type: "code"
-        }
-      }
+          response_type: "code",
+        },
+      },
     }),
     Auth0Provider({
       clientId: process.env.AUTH0_ID,
       clientSecret: process.env.AUTH0_SECRET,
-      issuer: process.env.AUTH0_ISSUER
+      issuer: process.env.AUTH0_ISSUER,
     }),
-    Credentials({
-      name: 'apiCredentials',
-      credentials: {
-      },
-      async authorize(credentials) {
-        const listUsers = await baseUrl.get('/userlogins/')
-        const isUserRegistered = listUsers.data.find(user => user.username === credentials.username)
-        if (!isUserRegistered) return;
-        return {
-          username: isUserRegistered.username,
-        }
-      }
-    })
   ],
-
-  callbacks: {
-    signIn: async (user) => {
-      const listUsers = await baseUrl.get('/userlogins/')
-      const isUserRegistered = listUsers.data.find(user => user.username === user.username)
-      if (!isUserRegistered) return;
-      return {
-        username: isUserRegistered.username,
-      }
-    }
-  }
-})
+});
