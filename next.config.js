206e1327073978b01856fb040c07fb0b4ca00a58
Richard Avendaño <abl.avn@gmail.com>
2022-06-06 18:41:59 -0500

    FIX: config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  trailingSlash: true
-}
-
-module.exports = nextConfig
-
-module.exports = {
+  trailingSlash: true,
   images: {
     domains: ['images.squarespace-cdn.com', 'images.unsplash.com', 'blush.design'],
   },
   BASE_URL_DEV: 'https://backend-ontologia.azurewebsites.net/api/',
   BASE_URL: 'https://localhost:7159/api'
 }
+
+module.exports = nextConfig
